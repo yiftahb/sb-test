3b5d2746819abc65d20c91573a9b0bb609bcc5eb
yiftahb <yiftah.b@gmail.com>
2015-10-13 15:53:01 +0200

    angular animate removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
 "use strict";
 
 // Define the application with its dependencies
-var app = angular.module('htmlParser', ['htmlParser.controllers', 'htmlParser.services', 'ngAnimate', 'ui.bootstrap']);
+var app = angular.module('htmlParser', ['htmlParser.controllers', 'htmlParser.services', 'ui.bootstrap']);
